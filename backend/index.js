73312c9705df51588879f43301de919cc2270adc
priyanshuparmar10 <priyanshuparmar2022@gmail.com>
2026-04-25 23:20:50 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://kirana-connect-murex.vercel.app", "http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
